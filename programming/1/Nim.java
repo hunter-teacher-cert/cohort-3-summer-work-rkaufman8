fc3212addedd050fb9eb664ffd88ffb0088f36ad
RACHEL KAUFMAN <71035742+rkaufman8@users.noreply.github.com>
2022-07-01 13:54:07 +0000

    *fixed Nim again

--- programming/1/Nim.java
+++ programming/1/Nim.java
@@ -6,12 +6,16 @@ public class Nim {
   public static void main(String[] args){
     int stones = 12;
     int stonesTaken = 0;
+
     boolean playerTurn = false;
+
+
 
     Scanner input = new Scanner(System.in);
 
     //loop until game ends
     while(stonesTaken < stones){
+
     
       playerTurn = !playerTurn;  
     int numStones;
@@ -40,5 +44,34 @@ public class Nim {
   
     System.out.println("Game over!");
     
+
+
+      System.out.print("Number of stones to take: ");
+        int numStones = input.nextInt(); 
+      stonesTaken += numStones;
+      System.out.println("Number of stones remaining: " + (stones - stonesTaken));
+
+      //check for win
+      if(stonesTaken >= stones){
+        System.out.println("User wins!");
+        break;
+      }
+      
+      //machine turn
+      Random r = new Random();
+      numStones = r.nextInt(3)+1;
+      System.out.println("Computer takes " + numStones + " stones.");
+      //calc number of stones remaining and print
+      stonesTaken += numStones;
+      System.out.println("Number of stones remaining: " + (stones - stonesTaken));
+      
+      //check for win
+      if(stonesTaken >= stones){
+        System.out.println("Computer wins!");
+        break;
+      }
+    }
+    System.out.println("Game over!");
+
   }
 }
