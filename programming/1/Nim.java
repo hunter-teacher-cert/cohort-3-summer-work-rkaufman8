26ad454d7cdd312fd58dab7f1eb2636118c22f7c
RACHEL KAUFMAN <71035742+rkaufman8@users.noreply.github.com>
2022-07-01 13:51:18 +0000

    updates to Nim, started day 2 folder

--- programming/1/Nim.java
+++ programming/1/Nim.java
@@ -6,37 +6,39 @@ public class Nim {
   public static void main(String[] args){
     int stones = 12;
     int stonesTaken = 0;
+    boolean playerTurn = false;
 
     Scanner input = new Scanner(System.in);
 
     //loop until game ends
     while(stonesTaken < stones){
-
-      System.out.print("Number of stones to take: ");
-        int numStones = input.nextInt(); 
-      stonesTaken += numStones;
-      System.out.println("Number of stones remaining: " + (stones - stonesTaken));
-
-      //check for win
-      if(stonesTaken >= stones){
-        System.out.println("User wins!");
-        break;
-      }
+    
+      playerTurn = !playerTurn;  
+    int numStones;
       
-      //machine turn
+    System.out.println("Number of stones remaining: " + (stones - stonesTaken));
+      
+      if (playerTurn) {
+        System.out.print("Number of stones you take: ");
+        numStones = input.nextInt();
+      } else {
+        //machine turn
       Random r = new Random();
       numStones = r.nextInt(3)+1;
       System.out.println("Computer takes " + numStones + " stones.");
-      //calc number of stones remaining and print
-      stonesTaken += numStones;
-      System.out.println("Number of stones remaining: " + (stones - stonesTaken));
-      
-      //check for win
-      if(stonesTaken >= stones){
-        System.out.println("Computer wins!");
-        break;
       }
+
+      stonesTaken += numStones;
+      
+      }
+    
+    if (playerTurn){
+      System.out.println("Player wins!");
+    } else {
+      System.out.println("Computer wins!");
     }
+  
     System.out.println("Game over!");
+    
   }
 }
